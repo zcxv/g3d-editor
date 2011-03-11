fc08a6684a659313afadcd74d75fc9aeae9e077e
patrickbiesenbach <patrickbiesenbach@yahoo.de>
2011-03-11 23:29:17 +0000

    G3D: * Various stuff

--- G3D-Editor/src/g3deditor/swing/DialogJumpTo.java
+++ G3D-Editor/src/g3deditor/swing/DialogJumpTo.java
@@ -17,10 +17,7 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
--- G3D-Editor/src/g3deditor/swing/Splash.java
+++ G3D-Editor/src/g3deditor/swing/Splash.java
@@ -1,8 +1,5 @@
 package g3deditor.swing;
 
-import g3deditor.geo.GeoBlockSelector;
-import g3deditor.geo.GeoEngine;
-import g3deditor.jogl.GLDisplay;
 import g3deditor.util.Util;
 
 import java.awt.AWTException;
@@ -13,17 +10,7 @@ import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.image.BufferedImage;
 
-import javax.media.opengl.GLCapabilities;
-import javax.media.opengl.GLProfile;
-import javax.media.opengl.awt.GLCanvas;
-import javax.swing.JPopupMenu;
 import javax.swing.JWindow;
-import javax.swing.ToolTipManager;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-
-import com.jogamp.opengl.util.FPSAnimator;
-import com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel;
 
 /**
  * 
@@ -38,20 +25,23 @@ public final class Splash extends JWindow implements Runnable
 		public boolean run();
 	}
 	
-	private final int _time;
+	private final long _time;
 	private final Runnable _r1;
 	private final CheckedRunnable _r2;
 	private final Runnable _r3;
 	
-	private Robot _robot;
 	private BufferedImage _image;
+	private BufferedImage _background;
 	private BufferedImage _buffer;
 	private AlphaComposite _composite;
+	
 	private float _alpha;
 	
-	public Splash(final int time, final Runnable r1, final CheckedRunnable r2, final Runnable r3)
+	public Splash(final long time, final Runnable r1, final CheckedRunnable r2, final Runnable r3)
 	{
-		_time = time;
+		setAlwaysOnTop(true);
+		
+		_time = System.currentTimeMillis() + time;
 		_r1 = r1;
 		_r2 = r2;
 		_r3 = r3;
@@ -60,23 +50,20 @@ public final class Splash extends JWindow implements Runnable
 		
 		if (_image != null)
 		{
+			setSize(_image.getWidth(), _image.getHeight());
+			setLocationRelativeTo(null);
+			
 			try
 			{
-				_robot = new Robot();
+				_background = new Robot().createScreenCapture(new Rectangle(getX(), getY(), _image.getWidth(), _image.getHeight()));
+				_buffer = new BufferedImage(_image.getWidth(), _image.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
+				_composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0F);
 			}
 			catch (final AWTException e)
 			{
 				
 			}
 			
-			if (_robot != null)
-			{
-				_buffer = new BufferedImage(_image.getWidth(), _image.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
-				_composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0F);
-			}
-			
-			setSize(_image.getWidth(), _image.getHeight());
-			setLocationRelativeTo(null);
 			setVisible(true);
 			
 			new Thread(this).start();
@@ -103,14 +90,11 @@ public final class Splash extends JWindow implements Runnable
 	
 	private final void drawImage(final float alpha)
 	{
-		if (_robot != null)
+		if (_background != null)
 		{
-			final BufferedImage background = _robot.createScreenCapture(new Rectangle(getX(), getY(), _image.getWidth(), _image.getHeight()));
 			final Graphics2D g = (Graphics2D) _buffer.getGraphics();
-			
-			g.clearRect(0, 0, _image.getWidth(), _image.getHeight());
 			g.setComposite(_composite);
-			g.drawImage(background, 0, 0, null);
+			g.drawImage(_background, 0, 0, null);
 			g.setComposite(_composite.derive(alpha));
 			g.drawImage(_image, 0, 0, null);
 	        
@@ -129,15 +113,13 @@ public final class Splash extends JWindow implements Runnable
 		{
 			while (_alpha < 1.0F)
 			{
-				_alpha += 0.05F;
+				_alpha += 0.025F;
 				if (_alpha > 1.0F)
 					_alpha = 1.0F;
 				
 				repaint();
-				Thread.sleep(_time / 100);
+				Thread.sleep(25);
 			}
-			
-			Thread.sleep(_time / 2);
 			
 			while (!_r2.run())
 			{
@@ -150,6 +132,12 @@ public final class Splash extends JWindow implements Runnable
 					
 				}
 			}
+			
+			final long sleep = _time - System.currentTimeMillis();
+			if (sleep > 0)
+				Thread.sleep(sleep);
+			
+			_r3.run();
 		}
 		catch (final Throwable t)
 		{
@@ -178,8 +166,6 @@ public final class Splash extends JWindow implements Runnable
 			{
 				
 			}
-			
-			_r3.run();
 		}
 	}
 	
